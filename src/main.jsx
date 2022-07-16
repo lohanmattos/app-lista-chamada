afd7c2b5736473be586af745b630d5308322b3a7
Lohan Amendola <lohanamendola18@gmail.com>
2022-07-15 20:59:04 -0400

    finished project

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ import "./styles/global.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Home/>
   </React.StrictMode>
 )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./styles.css"
 
 import Card from "../../components/Card"
@@ -11,10 +11,13 @@ function Home() {
   //Cria um array de estudantes
   const [students, setStudents] = useState([]);
 
+  //Cria um objeto de nome e avatar da api
+  const [user, setUser] = useState({ name: '', avatar: '' })
+
   function handleAddStudent() {
     const newStudent = {
       name: studentName,
-      time: new Date().toLocaleTimeString("pt-br",{
+      time: new Date().toLocaleTimeString("pt-br", {
         hour: '2-digit',
         minute: '2-digit',
         second: "2-digit"
@@ -23,12 +26,35 @@ function Home() {
 
     //cria um novo vetor com os dados anteriores
     setStudents(prevState => [...prevState, newStudent]);
-  
+
   }
+
+  useEffect(() => {
+    async function dataAPI(){
+      const url = "https://api.github.com/users/lohanmattos"
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setUser({
+        name: data.name,
+          avatar: data.avatar_url
+      })
+    }
+
+    dataAPI();
+  }, []);
 
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
+
+      <header>
+        <h1>Lista de Presença</h1>
+        <div>
+          <strong>{user.name}</strong>
+          <img src={user.avatar} alt="Foto de Perfil" />
+        </div>
+      </header>
+
 
       <input
         type="text"
@@ -41,11 +67,11 @@ function Home() {
       {
         //os cards ficam dinâmicos, pois para cada item da lista e gerado um card.
         students.map(student => (
-        <Card 
-          //Sempre utilizar uma key quando estiver utilizando uma estrutura de repetição. 
-          key={student.time}
-          name={student.name} 
-          time={student.time}/>
+          <Card
+            //Sempre utilizar uma key quando estiver utilizando uma estrutura de repetição. 
+            key={student.time}
+            name={student.name}
+            time={student.time} />
         ))
       }
 
